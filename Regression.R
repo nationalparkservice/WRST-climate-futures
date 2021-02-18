e09fa364df8521772e470e7a4d83d1503a014130
Annie Kellner <annie.kellner@colostate.edu>
2021-02-18 13:36:05 -0700

    create rasterstack

--- Regression.R
+++ Regression.R
@@ -28,7 +28,6 @@ pnt.list<-list.files(path=data.dir, pattern=".prcp.alaska") #list all files by v
 # Create list of tables
 
 tables <- list()
-test <- list()
 
 for(i in 1:length(pnt.list)){
   t = read.table(paste(data.dir, pnt.list[i], sep = '/'))
@@ -38,5 +37,42 @@ for(i in 1:length(pnt.list)){
   tables[[i]] = tt 
 }
 
+# Create raster list
+
+rasters <- list()
+
+for(i in 1:length(tables)) {
+  df <- tables[[i]]
+  coordinates(df) = ~Lon+Lat
+  proj4string(df) = "+proj=longlat +datum=WGS84 +no_defs " #same proj4string used in NPS_boundary_centroids.shp
+  df = spTransform(df, CRSobj = "+init=epsg:3338") #reproj sp obj
+  y = data.frame(df@coords)
+  y$PrecipC<-df$PrecipC
+  df = as.matrix(y)
+  e = extent(df[,1:2])
+  r =  raster(e, ncol=85, nrow=71)
+  x = rasterize(df[, 1:2], r, df[,3])
+  rasters[[i]] <- x
+}
+
+# Create raster stack
+
+st <- stack(rasters)
+  
+
+
+
+
+
+
+
+r <- raster(e, ncol=85, nrow=71)
+x <- rasterize(Dfx[, 1:2], r, Dfx[,3])
+plot(x)
+
+projection(x) = CRS("+init=epsg:3338")
+
+# Plot park over raster
+Sp_park<-spTransform(Sp_park,CRSobj = "+init=epsg:3338")
 
 
