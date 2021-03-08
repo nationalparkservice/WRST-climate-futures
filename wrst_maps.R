352e0e8aa2f1964c89de99c547f86019fa48cf70
Annie Kellner <annie.kellner@colostate.edu>
2021-03-07 21:39:00 -0700

    plotted basemap with park outline and raster data

--- wrst_maps.R
+++ wrst_maps.R
@@ -10,10 +10,13 @@ library(sp)
 library(sf)
 library(ggplot2)
 library(rasterVis)
+library(colorspace)
+library(RStoolbox)
 
 
 site = "WRST"
 aa <- CRS('+init=EPSG:3338') # Alaska Albers
+latlong = CRS('+init=EPSG:4326') # Lat/Long
 
 
 # Base map from Natural Earth - https://www.naturalearthdata.com/downloads/50m-cross-blend-hypso/50m-cross-blended-hypso-with-shaded-relief-and-water/
@@ -26,10 +29,15 @@ plotRGB(ak)
 
 ak2 <- projectRaster(ak, crs = aa)
 
+basemap <- ggRGB(ak2)
+
 # NPS Boundary
 
 nps_boundary <- st_read('./data/spatial-data/nps_boundary')
 park <- filter(nps_boundary, UNIT_CODE == site) # subset to WRST only
+park <- st_transform(park, crs = 3338)
+
+
 Sp_park <- as_Spatial(park)
 Sp_park<-spTransform(Sp_park,CRSobj = "+init=epsg:3338") 
 
@@ -47,11 +55,34 @@ ak_ca <- lines[2,]
 r <- raster('./output/rasters/Tmean_val.tif')
 plot(r)
 
-rev <- rasterTheme(region = rev(RdBuTheme))
+crs(r) <- aa
 
-levelplot(r, par.settings = r(RdBuTheme)
+# ggplot
+# color palette: https://colorspace.r-forge.r-project.org/articles/hcl_palettes.html#overview-1
+
+temp <- gplot(r) + geom_tile(aes(fill = value)) + 
+  scale_fill_continuous_divergingx(palette = "Earth") 
 
 
+
+basemap +
+  geom_sf(data = park) +
+  geom_tile(data = r, aes(fill = value)) + 
+  scale_fill_continuous_divergingx(palette = "Earth") 
+
+  
+
+
+
+
+map <- levelplot(r, par.settings = BuRdTheme, margin = FALSE, scales = list(draw = FALSE))  
+
+
+l <-levelplot(r, par.settings = BuRdTheme, margin = FALSE, scales = list(draw = FALSE))  
+
+plotRGB(ak2) + 
+  plot(r, col = hcl.colors(n = 7, palette = "viridis", alpha = 0.5), add = TRUE) + 
+  plot(Sp_park, add = TRUE)
 
 
 
