89e3dfbaa66681c3b97528cad95018c7d45c4120
Annie Kellner <annie.kellner@colostate.edu>
2021-03-08 09:46:10 -0700

    unable to plot title without compromising some other part of map

--- wrst_maps.R
+++ wrst_maps.R
@@ -30,12 +30,12 @@ plotRGB(ak)
 
 ak2 <- projectRaster(ak, crs = aa)
 
-basemap <- ggRGB(ak2)
+#basemap <- ggRGB(ak2)
 
 # NPS Boundary
 
 nps_boundary <- st_read('./data/spatial-data/nps_boundary')
-park <- filter(nps_boundary, UNIT_CODE == site) # subset to WRST only
+park <- dplyr::filter(nps_boundary, UNIT_CODE == site) # subset to WRST only
 park <- st_transform(park, crs = 3338)
 
 
@@ -47,9 +47,9 @@ plot(Sp_park, add = TRUE)
 
 # Boundaries
 
-lab <- st_read('./data/spatial-data/ne_10m_admin_0_boundary_lines_land')
-lines <- dplyr::filter(lab, adm0_right == "United States of America")
-ak_ca <- lines[2,]
+#lab <- st_read('./data/spatial-data/ne_10m_admin_0_boundary_lines_land')
+#lines <- dplyr::filter(lab, adm0_right == "United States of America")
+#ak_ca <- lines[2,]
 
 # Raster data
 
@@ -58,37 +58,19 @@ plot(r)
 
 crs(r) <- aa
 
-# ggplot
-# color palette: https://colorspace.r-forge.r-project.org/articles/hcl_palettes.html#overview-1
-
-temp <- gplot(r) + geom_tile(aes(fill = value)) + 
-  scale_fill_continuous_divergingx(palette = "Earth") 
-
-
-
-basemap +
-  geom_sf(data = park) +
-  geom_tile(data = r, aes(fill = value)) + 
-  scale_fill_continuous_divergingx(palette = "Earth") 
-
-  
-
-
-
-
-#map <- levelplot(r, par.settings = BuRdTheme, margin = FALSE, scales = list(draw = FALSE))  
-
-arrow <- layout.north.arrow(type = 2)
-Narrow1 <- maptools::elide(arrow, shift = c(extent(ak2)[2],extent(ak2)[3]))
-
+original_par <- par()
 
 png('./output/maps/Tave.png')
 
-plotRGB(ak2) 
-  plot(r, col = hcl.colors(n = 7, palette = "viridis", alpha = 0.5), legend = FALSE, add = TRUE) 
-  plot(Sp_park, add = TRUE) + 
-  plot(r, col = hcl.colors(n = 7, palette = "viridis", alpha = 0.5), legend.only = TRUE, horizontal = TRUE, legend.args = list(text = "Deg F", line = 1)) 
-  plot(Narrow1, add = TRUE)
+plotRGB(ak2, axes = TRUE, main = "Title") 
+par(col.axis = "white", col.lab = "white", tck = 0)
+box(col = "white")
+
+
+plot(r, col = hcl.colors(n = 7, palette = "viridis", alpha = 0.5), legend = FALSE, add = TRUE) 
+plot(Sp_park, add = TRUE) + 
+par(original_par)
+plot(r, col = hcl.colors(n = 7, palette = "viridis", alpha = 0.5), legend.only = TRUE, horizontal = TRUE, legend.args = list(text = "Deg F", line = 1)) 
 
 
 dev.off()
