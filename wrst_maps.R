6149ae1245d8bf2e3fbf6ed4e7486def8f526396
acrunyon <amber.c.runyon@gmail.com>
2021-03-09 09:42:22 -0700

    Update inputs and precip colors

--- wrst_maps.R
+++ wrst_maps.R
@@ -22,7 +22,8 @@ latlong = CRS('+init=EPSG:4326') # Lat/Long
 
 # Base map from Natural Earth - https://www.naturalearthdata.com/downloads/50m-cross-blend-hypso/50m-cross-blended-hypso-with-shaded-relief-and-water/
 
-topo <- stack('./data/spatial-data/HYP_HR_SR_W/HYP_HR_SR_W/HYP_HR_SR_W.tif') # read in as stack so can see RBG layers
+# topo <- stack('./data/spatial-data/HYP_HR_SR_W/HYP_HR_SR_W/HYP_HR_SR_W.tif') # read in as stack so can see RBG layers
+topo <- stack('C:/Users/achildress/DOI/NPS-NRSS-CCRP-FC Science Adaptation - Documents/General/RSS Stuff/Parks/WRST/2.0 CF and Scenario Development/Historical/HYP_HR_SR_W/HYP_HR_SR_W/HYP_HR_SR_W.tif') # read in as stack so can see RBG layers
 ext <- extent(-147, -139, 59.3, 63) # extent defined by lat/long
 
 ak <- crop(topo, ext)
@@ -34,7 +35,7 @@ ak2 <- projectRaster(ak, crs = aa)
 
 # NPS Boundary
 
-nps_boundary <- st_read('./data/spatial-data/nps_boundary')
+nps_boundary <- st_read('C:/Users/achildress/OneDrive - DOI/Documents/GIS/nps_boundary2018/nps_boundary.shp')
 park <- dplyr::filter(nps_boundary, UNIT_CODE == site) # subset to WRST only
 park <- st_transform(park, crs = 3338)
 
@@ -53,7 +54,7 @@ plot(Sp_park, add = TRUE)
 
 # Raster data
 
-r <- raster('./output/rasters/Tmin_val.tif')
+r <- raster('C:/Users/achildress/DOI/NPS-NRSS-CCRP-FC Science Adaptation - Documents/General/RSS Stuff/Parks/WRST/2.0 CF and Scenario Development/Historical/Rasters/precip_delta.tif')
 plot(r)
 
 crs(r) <- aa
@@ -63,15 +64,15 @@ crs(r) <- aa
 # Tave color palette = viridis (n = 7)
 # Tave delta = Lajolla (n = 7)
 # Precip mean = Oslo (n = 7)
-# Precip delta = Blues 3 (n = 4)
+# Precip delta = Blues 2 (n = 4)
 
 
-png('./output/maps/Tmin_val.png')
+png('C:/Users/achildress/DOI/NPS-NRSS-CCRP-FC Science Adaptation - Documents/General/RSS Stuff/Parks/WRST/2.0 CF and Scenario Development/Historical/Maps/Precip_delta.png')
 
 plotRGB(ak2) 
-plot(r, col = hcl.colors(n = 7, palette = "viridis", alpha = 0.5), legend = FALSE, add = TRUE) 
+plot(r, col = hcl.colors(n = 4, palette = "Blue-Yellow", alpha = 0.5,rev = TRUE),  legend = FALSE, add = TRUE) 
 plot(Sp_park, add = TRUE) + 
-plot(r, col = hcl.colors(n = 7, palette = "viridis", alpha = 0.5), legend.only = TRUE, horizontal = TRUE, legend.args = list(text = "Deg F", line = 1)) 
+plot(r, col = hcl.colors(n = 4, palette = "Blue-Yellow", alpha = 0.5, rev = TRUE), legend.only = TRUE, horizontal = TRUE, legend.args = list(text = "Precip (in)", line = 1)) 
 
 
 dev.off()
