891a280a8b89c5c185f6cf3a6a8c097d37d49dfe
acrunyon <amber.c.runyon@gmail.com>
2021-05-21 11:16:41 -0600

    data manipulation
    issue: think data on irregular grid b/c AK. Trouble converting for extraction

--- NCAR-parsing.R
+++ NCAR-parsing.R
@@ -1,48 +1,42 @@
 library(ncdf4)
 library(reshape2)
+library(raster)
+
 
 rm(list=ls())
+
+Lat = 62.23494
+Lon = -142.572
+
+#lat = 209 values; lon = 299 values
 
 # MET parsing
 data.dir<-"C:/Users/achildress/Documents/NCAR-test/met/"
 
 x<-nc_open(paste0(data.dir,"ACCESS1-3_rcp45_BCSD_met_1950.nc4"))
-x$dim$x
 
+# all_lat<-data.frame(x$dim$lat$vals)
 
-Lat = 33.791868
-Lon = -80.748665
-cLon = Lon + 360 #Adjusts negative lon. values 
+ncvar_get(x,"latitude")
 
-###Parameters for extracting data from NetCDF files
-#Specify top-level directory where MACA data is stored (all scenarios and variables)
-MACADir = "E:/ClimateData/MACAv2Metdata" #no '/' at the end
-setwd("C:/Users/adillon/Documents/RSS/CONG/MACA")
+Lat_index = as.numeric(which.min(abs(All_lat$nc1.dim.lat.vals - Lat)))
 
-#Variable and scenario names corresponding to MACA data directory structure
-vars = c("pr", "tasmax", "tasmin")
-scens = c("historical", "rcp45", "rcp85")
+lon <- ncvar_get(x, "longitude")
+lat <- ncvar_get(x, "latitude")
+la<-data.frame(lat) # row 150 is max, after that mirrors values
+lo<-data.frame(lon) #
 
-#Variable names for output tables
-VarNames = c("PrecipCustom", "TmaxCustom", "TminCustom","RHmaxCustom","RHminCustom")
+Lon_index = as.numeric(which.min(abs(lon - Lon)))
+Lat_index = as.numeric(which.min(abs(lat - Lat)))
 
-# GCMs to be extracted
-GCMs = c('bcc-csm1-1','bcc-csm1-1-m','BNU-ESM','CanESM2','CCSM4','CNRM-CM5','CSIRO-Mk3-6-0',
-         'GFDL-ESM2G','GFDL-ESM2M','HadGEM2-CC365','HadGEM2-ES365',
-         'inmcm4','IPSL-CM5A-MR','IPSL-CM5A-LR','IPSL-CM5B-LR',
-         'MIROC5','MIROC-ESM','MIROC-ESM-CHEM','MRI-CGCM3','NorESM1-M')
+y = ncvar_get(x, "tmax", c(Lon_index, Lat_index, 1))
 
-#Date ranges to be extracted
-Future_StartYear = 2006   #2006-2099
-Future_EndYear = 2099   #2006-2099
-Hist_StartYear = 1950     #1950-2005
-Hist_EndYear = 2005      #1950-2005
-HistYears = 50  
+y = brick(ncvar_get(x, "tmax"),crs="+init=epsg:3338")
+y.raster<-raster(y[[200]])
+crs(y) <- "+proj=longlat +datum=WGS84 +no_defs"
+crs(y) <- "+init=epsg:3338"
+projectRaster(y,crs = "+init=epsg:3338")
 
-#Directory where output tables will be saved
-OutDir = "C:/Users/adillon/Documents/RSS/CONG/MACA"
-
-
-
-
-
+rast.lon<-raster(paste0(data.dir,"ACCESS1-3_rcp45_BCSD_met_1950.nc4"),varname="longitude")
+y2<-projectRaster(y[[1]],crs = "+init=epsg:3338") #reproj sp obj
+y[[1]]
