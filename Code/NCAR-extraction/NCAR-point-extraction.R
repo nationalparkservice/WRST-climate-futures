4ade05c999eace843a980d5c149af023b16fecf2
acrunyon <amber.c.runyon@gmail.com>
2021-05-28 14:32:28 -0600

    Finish and test loop through RCP folders and files to create df

--- Code/NCAR-extraction/NCAR-point-extraction.R
+++ Code/NCAR-extraction/NCAR-point-extraction.R
@@ -14,9 +14,7 @@ rm(list=ls())
 ######### MET parsing - file location ######### 
 data.dir<-"C:/Users/achildress/Documents/NCAR-test/met/" #location data file
 
-#### MAKE SEPARATE 4.5 AND 8.5 FILE LISTS
-file.list<-list.files(path=data.dir, pattern=".nc4") #list all files by var
-
+RCPs <- c("rcp45", "rcp85")
 
 ######### Points for extraction ######### 
 Lat = 62.23494 #park centroid lat -- if doing multiple sites, read in points file, code below
@@ -29,24 +27,27 @@ cLon = Lon + 360 #Adjusts negative lon. values
 # Sp_park <- as_Spatial(park[1,])
 
 
-######### Parse model names ######### 
-# Extract model names from RCP 4.5, need to run loop through 4.5 and 8.5
-GCMs<-sub("\\_.*", "", file.list)
-
-
 ######### Create df for data ######### 
 # GCM, rcp, date, pcp, tmax, tmin
-data<-as.data.frame(matrix(data=NA,nrow=length(file.list),ncol=6))
-names(data)<-c("GCM","rcp","date","pcp","tmax","tmin")
+data<-as.data.frame(matrix(data=NA,nrow=0,ncol=6))
+names(data)<-c("Date","GCM","rcp","tmax","tmin","pcp")
+
+
+for (R in RCPs){
+rcp = RCPs[R]
+
+file.list<-list.files(path=paste0(data.dir, rcp), pattern=".nc4") #list all files in folder
+
+# Extract model names from RCP 
+GCMs<-sub("\\_.*", "", file.list) 
 
 ######## Method working with .nc object and 'flattening' for point location
-for (file in length(file.list))
-file=1
+for (file in length(file.list)){
 gcm = sub("\\_.*", "", file.list[file])
-year<-sub('.*[_]([^.]+)[.].*', "\\1", file.list[file]) #syntax explanation https://stackoverflow.com/questions/23518325/how-to-extract-substring-between-patterns-and-in-r
+# year<-sub('.*[_]([^.]+)[.].*', "\\1", file.list[file]) #syntax explanation https://stackoverflow.com/questions/23518325/how-to-extract-substring-between-patterns-and-in-r
 
 
-x<-nc_open(paste0(data.dir, file.list[file])) 
+x<-nc_open(paste0(paste0(data.dir, rcp, "/", file.list[file]))) 
 
 lon <- ncvar_get(x, "longitude") 
 lat <- ncvar_get(x, "latitude")
@@ -55,7 +56,14 @@ lat <- ncvar_get(x, "latitude")
 # it flattens the grid by converting coordinate to vectors
 
 ts<-as.POSIXct(nc.get.time.series(x))
-dum_var <- ncvar_get(x, req_var,start=c(1,1,1),count = c(-1,-1,1)) 
+df <- as.data.frame(matrix(NA,length(ts),0))  # dummy df
+df$Date <- ts
+
+df$GCM <- gcm
+df$rcp <- rcp
+
+
+dum_var <- ncvar_get(x, "tmax",start=c(1,1,1),count = c(-1,-1,1)) 
 coords<-data.frame(id=1:length(lon),lon=as.vector(lon), lat=as.vector(lat)) #index values match up to 
 
 coords$lat.distance<-abs(coords$lat - Lat)
@@ -68,19 +76,23 @@ Index = coords$id[which.min(coords$index)]
 Lat_index = as.numeric(which.min(abs(coords$lat - Lat))) #returns cell index that is closest to Lat var
 Lon_index = as.numeric(which.min(abs(coords$lon -cLon)))#returns cell index that is closest to Lon var
 
-req_var<-names(x$var)[4] #tmax UPDATE THIS TO 
-
   #convert linear ind to r and c index
   rc <- arrayInd(Index,dim(dum_var)) #get row and column index
   
-  #get the variable at the required location for all time steps.
-  
-  pt_data <- ncvar_get(x, req_var,  
+  #get the variables at the required location for all time steps.
+  df$tmax<- ncvar_get(x, "tmax",  
                        start=c(rc[1],rc[2],1),count=c(1,1,-1)) #-1 read all time_steps
+  df$tmin<- ncvar_get(x, "tmin",  
+                   start=c(rc[1],rc[2],1),count=c(1,1,-1)) #-1 read all time_steps
+  df$pcp <- ncvar_get(x, "pcp",  
+                   start=c(rc[1],rc[2],1),count=c(1,1,-1)) #-1 read all time_steps
   
   # add other variables in output here
-data<-rbind(data,file.data)
+data<-rbind(data,df)
 
 nc_close(x)
 }
+}
+rm(df)
 
+data$GCM <- paste(data$GCM, data$rcp, sep=".")
