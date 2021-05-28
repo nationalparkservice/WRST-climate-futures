8eeb36d7cc0dd62a83f05b2fde94b819da411476
acrunyon <amber.c.runyon@gmail.com>
2021-05-28 13:58:48 -0600

    adding elements to loop over all files in data.dir

--- Code/NCAR-extraction/NCAR-point-extraction.R
+++ Code/NCAR-extraction/NCAR-point-extraction.R
@@ -11,32 +11,49 @@ library(units) # for dropping units
 
 rm(list=ls())
 
-Lat = 62.23494 #park centroid lat -- if doing multiple sites, read in points file
+######### MET parsing - file location ######### 
+data.dir<-"C:/Users/achildress/Documents/NCAR-test/met/" #location data file
+
+#### MAKE SEPARATE 4.5 AND 8.5 FILE LISTS
+file.list<-list.files(path=data.dir, pattern=".nc4") #list all files by var
+
+
+######### Points for extraction ######### 
+Lat = 62.23494 #park centroid lat -- if doing multiple sites, read in points file, code below
 Lon = -142.572 #park centroid lon
 cLon = Lon + 360 #Adjusts negative lon. values 
 
-# read in parks shapefile
-nps_boundary <- st_read('C:/Users/achildress/OneDrive - DOI/Documents/GIS/nps_boundary_centroids/nps_boundary_centroids.shp')
-park <- filter(nps_boundary, UNIT_CODE == "WRST") # subset to WRST only
-Sp_park <- as_Spatial(park[1,])
+# # Use this code when reading in point locations to extract from
+# nps_boundary <- st_read('C:/Users/achildress/OneDrive - DOI/Documents/GIS/nps_boundary_centroids/nps_boundary_centroids.shp')
+# park <- filter(nps_boundary, UNIT_CODE == "WRST") # subset to WRST only
+# Sp_park <- as_Spatial(park[1,])
 
 
-# MET parsing
-data.dir<-"C:/Users/achildress/Documents/NCAR-test/met/" #location data file
+######### Parse model names ######### 
+# Extract model names from RCP 4.5, need to run loop through 4.5 and 8.5
+GCMs<-sub("\\_.*", "", file.list)
 
-######## Method working with .nc object and 'flattening' 
-x<-nc_open(paste0(data.dir,"ACCESS1-3_rcp45_BCSD_met_1950.nc4")) 
 
-lon <- ncvar_get(x, "longitude") # read in lon
+######### Create df for data ######### 
+# GCM, rcp, date, pcp, tmax, tmin
+data<-as.data.frame(matrix(data=NA,nrow=length(file.list),ncol=6))
+names(data)<-c("GCM","rcp","date","pcp","tmax","tmin")
+
+######## Method working with .nc object and 'flattening' for point location
+for (file in length(file.list))
+file=1
+gcm = sub("\\_.*", "", file.list[file])
+year<-sub('.*[_]([^.]+)[.].*', "\\1", file.list[file]) #syntax explanation https://stackoverflow.com/questions/23518325/how-to-extract-substring-between-patterns-and-in-r
+
+
+x<-nc_open(paste0(data.dir, file.list[file])) 
+
+lon <- ncvar_get(x, "longitude") 
 lat <- ncvar_get(x, "latitude")
-tmax <- ncvar_get(x, "tmax") #working w/ tmax b/c pcp returns too many 0s
-tmax[200,73,] #returns tmax data for lon 200,lat 73, all time (365)
-
-
+ 
 # this chunk of code from Method 2, here https://gis.stackexchange.com/questions/390148/handle-curvilinear-rotated-grid-netcdf-file-in-r
 # it flattens the grid by converting coordinate to vectors
-names(x$var)
-req_var<-names(x$var)[4] #tmax
+
 ts<-as.POSIXct(nc.get.time.series(x))
 dum_var <- ncvar_get(x, req_var,start=c(1,1,1),count = c(-1,-1,1)) 
 coords<-data.frame(id=1:length(lon),lon=as.vector(lon), lat=as.vector(lat)) #index values match up to 
@@ -50,15 +67,16 @@ Index = coords$id[which.min(coords$index)]
 
 Lat_index = as.numeric(which.min(abs(coords$lat - Lat))) #returns cell index that is closest to Lat var
 Lon_index = as.numeric(which.min(abs(coords$lon -cLon)))#returns cell index that is closest to Lon var
-# Note these don't work because data are a matrix, not index so don't identify same cell
 
-data_out <- rep(0,length(ts)) #create zeros vector to output the average value
+file.data <- rep(0,length(ts)) #create zeros vector to output the average value
 
 id <-c(Index) # c(Lat_index,Lon_index) # index of cells within the mask shapefile -- single value if single cell, multi values if multi cells
 
-for (i in 1:length(id)) {
+req_var<-names(x$var)[4] #tmax UPDATE THIS TO 
+
+for (j in 1:length(id)) {
   
-  pt_id <- id[i]
+  pt_id <- id[j]
   #convert linear ind to r and c index
   rc <- arrayInd(pt_id,dim(dum_var)) #get row and column index
   
@@ -66,9 +84,11 @@ for (i in 1:length(id)) {
   
   pt_data <- ncvar_get(x, req_var,  
                        start=c(rc[1],rc[2],1),count=c(1,1,-1)) #-1 read all time_steps
-  data_out <- data_out+pt_data #sum data to be averaged after the loop ## Doesn't work if NA values
+  file.data <- file.data+pt_data #sum data to be averaged after the loop ## Doesn't work if NA values
+  # add other variables in output here
 }
-data_out
+data<-rbind(data,file.data)
 
 nc_close(x)
+}
 
