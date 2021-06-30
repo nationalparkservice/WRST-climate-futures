093a8c1dbd5beeed340141f5fc348e9017d14eda
Annie Kellner <annie.kellner@colostate.edu>
2021-06-30 11:59:24 -0600

    initial commit

---
+++ Code/Create_stars_objects_for_plots.R
@@ -0,0 +1,53 @@
+############################################################################
+##    CREATE STARS OBJECTS FOR T1 PLOTS   ##################################
+############################################################################
+
+# HISTORICAL
+
+l <- list() # Create a list to put the stars objects into
+
+for(i in 1:length(hist_filelist)){
+  l[[i]] <- read_ncdf(hist_filelist[i], curvilinear = c("longitude", "latitude")) # need to read in as ncdf or coordinate system does not translate (not sure why)
+}
+
+# Crop
+
+cropped_hist <- list() # create list for cropped stars objects
+
+for(i in 1:length(l)){ # add cropped stars objects to a new list
+  nc = l[[i]]
+  nc = st_transform(nc, st_crs(wrst))
+  nc_crop = nc[wrst]
+  cropped_hist[[i]] = nc_crop
+}
+
+cropped_st_hist <- list()
+
+for(i in 1:length(cropped_hist)){
+  cropped_st_hist[[i]] <- st_as_stars(cropped_hist[[i]])
+}
+
+# FUTURE
+
+l <- list() # Create a list to put the stars objects into
+
+for(i in 1:length(fut_filelist)){
+  l[[i]] <- read_ncdf(fut_filelist[i], curvilinear = c("longitude", "latitude")) # need to read in as ncdf or coordinate system does not translate (not sure why)
+}
+
+# Crop
+
+cropped_fut <- list() # create list for cropped stars objects
+
+for(i in 1:length(l)){ # add cropped stars objects to a new list
+  nc = l[[i]]
+  nc = st_transform(nc, st_crs(wrst))
+  nc_crop = nc[wrst]
+  cropped_fut[[i]] = nc_crop
+}
+
+cropped_st_fut <- list()
+
+for(i in 1:length(cropped_fut)){
+  cropped_st_fut[[i]] <- st_as_stars(cropped_fut[[i]])
+}
