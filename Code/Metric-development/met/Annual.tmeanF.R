45453eaec823772767b334f872786a2f6aca027c
acrunyon <amber.c.runyon@gmail.com>
2021-07-22 18:48:04 -0600

    Annual.tmeanF extract annual mean and final grid for daymet - working

--- Code/Metric-development/met/Annual.tmeanF.R
+++ Code/Metric-development/met/Annual.tmeanF.R
@@ -13,16 +13,32 @@ grid_var_stars <- Reduce(c, grid_var)
 grid_var_stars$tmean <- drop_units(grid_var_stars$tmean)
 grid_var_stars %>% mutate(tmean_f = tmean * 9/5 + 32) %>% select(tmean_f) -> grid_var_stars
 
-mean_grid <- st_apply(grid_var_stars, c("x", "y"), mean)
 
-grid_var_stars %>% mutate()
-mutate(year = year(time)) %>%
-  group_by(year) %>%
-  summarise(Annual.tmean = mean(tmean_f,na.rm=TRUE)) %>%
-  as.data.frame() -> grid_df
+# st_get_dimension_values(grid_var_stars,"time") #how get time dimension values
+
+by_t = "1 year"
+test <- aggregate(grid_var_stars, by = by_t, FUN = mean, na.omit = TRUE) # Doesn't work in lat/long. Must be projected. Removes units from tmax. Also aggregates to a lower resolution.
+test2 <- split(test, "time")
+
+df<-data.frame(year=daymet.period,mean=NA)
+for (i in 1:length(daymet.period)){
+t <-st_apply(test2[i],1:2,mean)
+df$mean[i] <- mean(t$mean,na.rm=TRUE)
+}
+
+# test2 <- split(test, "time") # ggplot will not work if time is a dimension, so switching to an attribute. Should not matter since time is aggregated here. 
+mean_grid <- st_apply(test, c("x", "y"), mean)
+saveRDS(mean_grid, file = paste0(model.dir,"/",var,gcm))
+
+# Plot
+
+ggplot() + # Resolution is course
+  geom_stars(data = mean_grid, alpha = 0.8) + 
+  # facet_wrap("time") +
+  # scale_fill_viridis() + 
+  #coord_equal() + 
+  theme_map() +
+  theme(legend.position = "bottom") +
+  theme(legend.key.width = unit(2, "cm"))
 
 
-grid_df <- as_tibble(grid_var_stars) %>% mutate(year = as.factor(year(time))) %>%
-  group_by(year) #%>% summarise(Annual.tmean = mean(tmean_f,na.rm=TRUE))
-
-mean_annual 
