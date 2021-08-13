fea34aa830711956558abc6db64ec59e66575300
acrunyon <amber.c.runyon@gmail.com>
2021-08-13 00:51:31 -0600

    update data.dir; add pr>0.5

--- Code/Metric-development/met/met-daily-stars_by_year.R
+++ Code/Metric-development/met/met-daily-stars_by_year.R
@@ -1,9 +1,7 @@
 ## MET MONTHLY
 print("extracting Daymet")
+DF = data.frame()
 
-model.dir <- paste0(data.dir,"/", "Daymet")
-# dir.create(model.dir,showWarnings=FALSE)
-DF.grid <- data.frame()
 grid_filelist = list.files(path = paste(met.dir,"daymet",sep='/'), pattern= '.nc', full.names = TRUE)
 
 # DAYMET ----
@@ -12,7 +10,7 @@ for(i in 1:length(grid_filelist)){
 yr = as.POSIXct(sub('.*\\met_', '', sub("\\..*", "", grid_filelist[i])),format="%Y")
 invisible(capture.output(
   suppressWarnings(
-    grid_star = read_stars(grid_filelist[i], sub=c("tmax","tmin","pcp") ,curvilinear = c("longitude", "latitude")))))
+    (grid_star = read_stars(grid_filelist[i], sub=c("tmax","tmin","pcp") ,curvilinear = c("longitude", "latitude"))))))
 grid_star = st_transform(grid_star, st_crs(shp))
 grid_crop = grid_star[shp]
 grid_crop = drop_units(grid_crop)
@@ -27,7 +25,8 @@ grid_crop %>% mutate(tmax_f = tmax * 9/5 + 32) %>%
 grid_crop %>% mutate(freeze.thaw = tmax_f > 34 & tmin_f < 28) %>%
   mutate(GDD = (tmax_f + tmin_f)/2 - 0 ) %>%
   mutate(under32 = tmin < 0) %>%
-  mutate(over20 = tmax > 20)-> grid_threshold
+  mutate(over20 = tmax > 20) %>%
+  mutate(pcp.over.5 = pcp_in > 0.5) -> grid_threshold
   # mutate(month = as.numeric(format(st_get_dimension_values(grid_crop, 'time'),"%m"))) 
   # mutate(hot.pctl = quantile(tmax_f,.99,na.rm=TRUE)) 
 
@@ -35,7 +34,7 @@ freeze.thaw.sum <- st_apply((grid_threshold %>% dplyr::select(freeze.thaw)), c("
 GDD.sum <- st_apply((grid_threshold %>% dplyr::select(GDD)), c("x", "y"), sum,rename=FALSE)
 under32.sum <- st_apply((grid_threshold %>% dplyr::select(under32)), c("x", "y"), sum,rename=FALSE)
 over20.sum <- st_apply((grid_threshold %>% dplyr::select(over20)), c("x", "y"), sum,rename=FALSE)
-
+pcp.over.5.sum <- st_apply((grid_threshold %>% dplyr::select(pcp.over.5)), c("x", "y"), sum,rename=FALSE)
 # Don't know how to add/retain time dimension for year
 
 by_t = "1 month"
@@ -47,8 +46,7 @@ names(WSF.below32) <- "WSF.below32"
 W.under32 <- st_apply(under32.month[,c(1:2,12)],c("x", "y"),sum,rename=FALSE)
 names(W.under32) <- "W.under32"
 
-# grid_annual[[i]]<- 
-annual_thresholds <- c(freeze.thaw.sum, GDD.sum,under32.sum,over20.sum,WSF.below32,W.under32)
+annual_thresholds <- c(freeze.thaw.sum, GDD.sum,under32.sum,over20.sum,WSF.below32,W.under32,pcp.over.5.sum)
 
 ## timeseries df
 df<-data.frame(GCM = "Daymet",year = yr)
@@ -56,20 +54,17 @@ s <- st_apply(annual_thresholds,1:2,mean)
 df$freeze.thaw = mean(s$freeze.thaw,na.rm=TRUE)
 df$GDD = mean(s$GDD,na.rm=TRUE)
 df$under32 = mean(s$under32,na.rm=TRUE)
-df$over20 = mean(s$over20)
+df$over20 = mean(s$over20,na.rm=TRUE)
 df$WSF.below32 = mean(s$WSF.below32,na.rm=TRUE)
 df$W.under32 = mean(s$W.under32,na.rm=TRUE)
+df$pcp.over.5 = mean(s$pcp.over.5,na.rm=TRUE)
 
-DF.grid[[i]] <- df
-rm(annual_thresholds,freeze.thaw.sum, GDD.sum,under32.sum,Over20.sum,WSF.below32,W.under32,
+DF = rbind(DF, df)
+rm(annual_thresholds,freeze.thaw.sum, GDD.sum,under32.sum,Over20.sum,WSF.below32,W.under32,pcp.over.5.sum
    grid_threshold,grid_crop)
 gc()
 }
-write.csv(DF.grid,paste0(model.dir,"/Annual_thresholds_Daymet.csv"),row.names = TRUE)
 
-
-
-DF.fut <- data.frame()
 for (G in 1:length(GCMs)){
   # setting variables ----
   gcm = sub("\\..*", "", GCMs[G])
@@ -78,7 +73,7 @@ for (G in 1:length(GCMs)){
     file.list = list.files(path = path, pattern = '.nc4', full.names = TRUE)
     fut_filelist = Filter(function(x) grepl(paste(future.period, collapse = "|"), x), file.list)
     
-    model.dir <- paste0(data.dir,"/",gcm,".",rcp)
+    # model.dir <- paste0(data.dir,"/",gcm,".",rcp)
     # dir.create(model.dir,showWarnings=FALSE)
     
     print(paste0("extracting ", GCMs[G]))
@@ -90,9 +85,9 @@ for (G in 1:length(GCMs)){
     for(i in 1:length(future_filelist)){
       yr = as.POSIXct(sub('.*\\met_', '', sub("\\..*", "", fut_filelist[i])),format="%Y")
       print(yr)
-      # invisible(capture.output(
-      #   suppressWarnings(
-      fut_star = read_ncdf(fut_filelist[i], curvilinear = c("longitude", "latitude"))
+      invisible(capture.output(
+        suppressWarnings(
+      (fut_star = read_ncdf(fut_filelist[i], var=c("tmax","tmin","pcp"), curvilinear = c("longitude", "latitude"))))))
       fut_star = st_transform(fut_star, st_crs(shp))
       fut_crop = fut_star[shp]
       fut_crop = drop_units(fut_crop)
@@ -107,12 +102,14 @@ for (G in 1:length(GCMs)){
       fut_crop %>% mutate(freeze.thaw = tmax_f > 34 & tmin_f < 28) %>%
         mutate(GDD = (tmax_f + tmin_f)/2 - 0 ) %>%
         mutate(under32 = tmin < 0) %>%
-        mutate(over20 = tmax > 20) -> fut_threshold
+        mutate(over20 = tmax > 20) %>%
+        mutate(pcp.over.5 = pcp_in > 0.5) -> fut_threshold
 
       freeze.thaw.sum <- st_apply((fut_threshold %>% dplyr::select(freeze.thaw)), c("x", "y"), sum, rename=FALSE) 
       GDD.sum <- st_apply((fut_threshold %>% dplyr::select(GDD)), c("x", "y"), sum,rename=FALSE)
       under32.sum <- st_apply((fut_threshold %>% dplyr::select(under32)), c("x", "y"), sum,rename=FALSE)
       over20.sum <- st_apply((fut_threshold %>% dplyr::select(over20)), c("x", "y"), sum,rename=FALSE)
+      pcp.over.5.sum <- st_apply((fut_threshold %>% dplyr::select(pcp.over.5)), c("x", "y"), sum,rename=FALSE)
       
       # Don't know how to add/retain time dimension for year
       
@@ -125,10 +122,7 @@ for (G in 1:length(GCMs)){
       W.under32 <- st_apply(under32.month[,c(1:2,12)],c("x", "y"),sum,rename=FALSE)
       names(W.under32) <- "W.under32"
       
-      
-      # hist_annual[[i]]<- 
-        annual_thresholds <- c(freeze.thaw.sum, GDD.sum,under32.sum,over20.sum,WSF.below32,W.under32)
-      
+      annual_thresholds <- c(freeze.thaw.sum, GDD.sum,under32.sum,over20.sum,WSF.below32,W.under32,pcp.over.5.sum )
       
       ## timeseries df
       df<-data.frame(GCM = GCMs[G],year = yr)
@@ -139,13 +133,16 @@ for (G in 1:length(GCMs)){
       df$over20 = mean(s$over20,na.rm=TRUE)
       df$WSF.below32 = mean(s$WSF.below32,na.rm=TRUE)
       df$W.under32 = mean(s$W.under32,na.rm=TRUE)
+      df$pcp.over.5 = mean(s$pcp.over.5,na.rm=TRUE)
 
-      DF.fut <- rbind(DF.fut,df)
+      DF <- rbind(DF,df)
       
-      rm(annual_thresholds,freeze.thaw.sum, GDD.sum,under32.sum,Over20.sum,WSF.below32,W.under32,
+      rm(annual_thresholds,freeze.thaw.sum, GDD.sum,under32.sum,over20.sum,WSF.below32,W.under32,pcp.over.5.sum,
          fut_threshold,fut_crop)
       gc()
       # )
 }
-write.csv(DF.fut,paste0(data.dir,"/Annual_thresholds_",gcm,"_",rcp,".csv"),row.names = TRUE)
 }
+
+write.csv(DF,paste0(data.dir,"/Daily_met.csv"),row.names=FALSE)
+
