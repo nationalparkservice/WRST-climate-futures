cda90432308aee01993d049824c9d90efa338ccd
acrunyon <amber.c.runyon@gmail.com>
2021-08-12 18:24:23 -0600

    Remove historical parsing; update output to data.dir

--- Code/Metric-development/met/met-monthly-stars.R
+++ Code/Metric-development/met/met-monthly-stars.R
@@ -5,11 +5,8 @@ for (G in 1:length(GCMs)){
     rcp = sub('.*\\.', '', GCMs[G])
     path = paste(met.dir,"monthly/BCSD", gcm, rcp, sep = '/')
     file.list = list.files(path = path, pattern = '.nc', full.names = TRUE)
-    hist_filelist = Filter(function(x) grepl(paste(historical.period, collapse = "|"), x), file.list)
+    # hist_filelist = Filter(function(x) grepl(paste(historical.period, collapse = "|"), x), file.list)
     fut_filelist = Filter(function(x) grepl(paste(future.period, collapse = "|"), x), file.list)
-    
-    model.dir <- paste0(data.dir,"/",gcm,".",rcp)
-    dir.create(model.dir,showWarnings=FALSE)
     
     print(paste0("extracting ", GCMs[G]))
 
@@ -25,27 +22,27 @@ for (G in 1:length(GCMs)){
       )
     }
     
-    # Crop
-    
-    cropped_hist <- list() # create list for cropped stars objects
-    
-    for(i in 1:length(l)){ # add cropped stars objects to a new list
-      nc = l[[i]]
-      nc = st_transform(nc, st_crs(shp))
-      nc_crop = nc[shp]
-      cropped_hist[[i]] = nc_crop
-    }
-    
-    cropped_st_hist <- list()
-    
-    for(i in 1:length(cropped_hist)){
-      cropped_st_hist[[i]] <- st_as_stars(cropped_hist[[i]])
-    }
-    
-    # assign(paste0("cropped_st_hist_",GCMs[G]), cropped_st_hist)
-    saveRDS(cropped_st_hist, file = paste(model.dir,paste0("cropped_st_hist_",gcm,"_",rcp),sep="/"))
-    
-    
+    # # Crop
+    # 
+    # cropped_hist <- list() # create list for cropped stars objects
+    # 
+    # for(i in 1:length(l)){ # add cropped stars objects to a new list
+    #   nc = l[[i]]
+    #   nc = st_transform(nc, st_crs(shp))
+    #   nc_crop = nc[shp]
+    #   cropped_hist[[i]] = nc_crop
+    # }
+    # 
+    # cropped_st_hist <- list()
+    # 
+    # for(i in 1:length(cropped_hist)){
+    #   cropped_st_hist[[i]] <- st_as_stars(cropped_hist[[i]])
+    # }
+    # 
+    # # assign(paste0("cropped_st_hist_",GCMs[G]), cropped_st_hist)
+    # saveRDS(cropped_st_hist, file = paste(model.dir,paste0("cropped_st_hist_",gcm,"_",rcp),sep="/"))
+    # 
+    # 
       # FUTURE ----
     
     l <- list() # Create a list to put the stars objects into
@@ -73,12 +70,10 @@ for (G in 1:length(GCMs)){
       cropped_st_fut[[i]] <- st_as_stars(cropped_fut[[i]])
     }
     # assign(paste0("cropped_st_fut_",GCMs[G]), cropped_st_fut)
-    saveRDS(cropped_st_fut, file = paste(model.dir,paste0("cropped_st_fut_",gcm,"_",rcp),sep="/"))
+    saveRDS(cropped_st_fut, file = paste(data.dir,paste0("cropped_st_fut_",gcm,"_",rcp),sep="/"))
 }
 print("extracting Daymet")
 
-model.dir <- paste0(data.dir,"/", "Daymet")
-dir.create(model.dir,showWarnings=FALSE)
 
 grid_filelist = list.files(path = paste(met.dir,"monthly/daymet",sep='/'), pattern= '.nc', full.names = TRUE)
 
@@ -110,7 +105,7 @@ for(i in 1:length(cropped_grid)){
   cropped_st_grid[[i]] <- st_as_stars(cropped_grid[[i]])
 }
 # assign(paste0("cropped_st_grid_",GCMs[G]), cropped_st_grid)
-saveRDS(cropped_st_grid, file = paste(model.dir,"cropped_st_Daymet",sep="/"))
+saveRDS(cropped_st_grid, file = paste(data.dir,"cropped_st_Daymet",sep="/"))
 
 
 rm(cropped_st_grid,cropped_st_fut,cropped_fut,cropped_grid,cropped_hist,nc_crop,nc,l,nc,s)
