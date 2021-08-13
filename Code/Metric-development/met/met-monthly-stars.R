3fbd9354f0d40b78880ead88e8a4672b19ebaa63
acrunyon <amber.c.runyon@gmail.com>
2021-08-12 21:12:19 -0600

    add historical back in

--- Code/Metric-development/met/met-monthly-stars.R
+++ Code/Metric-development/met/met-monthly-stars.R
@@ -5,44 +5,44 @@ for (G in 1:length(GCMs)){
     rcp = sub('.*\\.', '', GCMs[G])
     path = paste(met.dir,"monthly/BCSD", gcm, rcp, sep = '/')
     file.list = list.files(path = path, pattern = '.nc', full.names = TRUE)
-    # hist_filelist = Filter(function(x) grepl(paste(historical.period, collapse = "|"), x), file.list)
+    hist_filelist = Filter(function(x) grepl(paste(historical.period, collapse = "|"), x), file.list)
     fut_filelist = Filter(function(x) grepl(paste(future.period, collapse = "|"), x), file.list)
     
     print(paste0("extracting ", GCMs[G]))
 
     # Createing stars objects ####
-    
+
       # HISTORICAL ----
-    # 
-    # l <- list() # Create a list to put the stars objects into
-    # 
-    # for(i in 1:length(hist_filelist)){
-    #   suppressMessages(
-    #   l[[i]] <- read_ncdf(hist_filelist[i], curvilinear = c("longitude", "latitude")) # need to read in as ncdf or coordinate system does not translate (not sure why)
-    #   )
-    # }
-    # 
-    # # Crop
-    # 
-    # cropped_hist <- list() # create list for cropped stars objects
-    # 
-    # for(i in 1:length(l)){ # add cropped stars objects to a new list
-    #   nc = l[[i]]
-    #   nc = st_transform(nc, st_crs(shp))
-    #   nc_crop = nc[shp]
-    #   cropped_hist[[i]] = nc_crop
-    # }
-    # 
-    # cropped_st_hist <- list()
-    # 
-    # for(i in 1:length(cropped_hist)){
-    #   cropped_st_hist[[i]] <- st_as_stars(cropped_hist[[i]])
-    # }
-    # 
-    # # assign(paste0("cropped_st_hist_",GCMs[G]), cropped_st_hist)
-    # saveRDS(cropped_st_hist, file = paste(model.dir,paste0("cropped_st_hist_",gcm,"_",rcp),sep="/"))
-    # 
-    # 
+
+    l <- list() # Create a list to put the stars objects into
+
+    for(i in 1:length(hist_filelist)){
+      suppressMessages(
+      l[[i]] <- read_ncdf(hist_filelist[i], curvilinear = c("longitude", "latitude")) # need to read in as ncdf or coordinate system does not translate (not sure why)
+      )
+    }
+
+    # Crop
+
+    cropped_hist <- list() # create list for cropped stars objects
+
+    for(i in 1:length(l)){ # add cropped stars objects to a new list
+      nc = l[[i]]
+      nc = st_transform(nc, st_crs(shp))
+      nc_crop = nc[shp]
+      cropped_hist[[i]] = nc_crop
+    }
+
+    cropped_st_hist <- list()
+
+    for(i in 1:length(cropped_hist)){
+      cropped_st_hist[[i]] <- st_as_stars(cropped_hist[[i]])
+    }
+
+    # assign(paste0("cropped_st_hist_",GCMs[G]), cropped_st_hist)
+    saveRDS(cropped_st_hist, file = paste(model.dir,paste0("cropped_st_hist_",gcm,"_",rcp),sep="/"))
+
+
       # FUTURE ----
     
     l <- list() # Create a list to put the stars objects into
