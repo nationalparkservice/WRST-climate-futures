4bb81aea70f2ec020a78151dbd14a2056dbaf8f4
acrunyon <amber.c.runyon@gmail.com>
2021-09-13 10:55:45 -0600

    functions for maps

--- Code/Plots/SWE.precip.maps.R
+++ Code/Plots/SWE.precip.maps.R
@@ -3,6 +3,9 @@ working.dir <- "C:/Users/achildress/Documents/wrst_temp"
 area="WRST_simple"
 data.dir <- paste(working.dir, "Data", area, sep="/")
 
+col.ramp = c("mediumseagreen","#fed976", "#fd8d3c", "#e31a1c", "#c6dbef",
+             "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#084594")
+
 # Need to recalculate SWE.precip with abs for each CF and Daymet for wrst-simple
 
 # read in WRST-simple SWE.precip abs
@@ -24,32 +27,27 @@ boundary.dir <- "C:/Users/achildress/DOI/NPS-WRST-Resource Stewardship Strategy 
 wrst <- st_read(paste0(boundary.dir, "wrst_simple.shp")) # Wrangell Mountains
 wrst <- st_transform(wrst, 3338)
 
-# dem = raster(paste0(boundary.dir,"/DEM/dem_3338")) # crs = 'SR-ORG:80'
-# projectRaster(dem,wrst)
+summarize_cf <- function(in_stars,out_stars){
+  mean_stars <- st_apply(in_stars, c("x", "y"), mean)
+  
+  mean_stars %>% 
+    mutate(categories = round(mean,digits=1),
+           cat_factor = factor(categories, levels=seq(0.1,1,0.1))) %>% 
+    dplyr::select(cat_factor) -> out_stars
+}
 
-# ak_df  <- as.data.frame(ak2, xy = TRUE) # this step is important to get it to plot in ggplot
-
-mean_grid<-st_apply(Hist, c("x", "y"), mean)
-
-mean_grid %>% 
-  mutate(categories = round(mean,digits=1),
-    cat_factor = factor(categories, levels=seq(0.1,1,0.1))) %>% 
-    dplyr::select(cat_factor) -> cat
-
-# Split by geog, add in DEM, convert to df
-
-# fill plot (heatmap?) with aes(x=time,y=elev,fill=SWE.p)
-
-col.ramp = c("mediumseagreen","#fed976", "#fd8d3c", "#e31a1c", "#c6dbef",
-             "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#084594")
+hist_cat <- summarize_cf(hist,hist_cat)
+cf1_cat <- summarize_cf(cf1, cf1_cat)
+cf2_cat <- summarize_cf(cf2, cf2_cat)
+cf3_cat <- summarize_cf(cf3, cf3_cat)
 
 # ggplot
-# map.plot <- function(data, title,xaxis,metric,col){
+map.plot <- function(data, title,bkd_col){
   ggplot() + 
-    # geom_raster(data = ak_df ,aes(x = x, y = y,alpha=HYP_HR_SR_W.1), show.legend=FALSE) +
-    geom_stars(data = cat, alpha = 0.8) + 
+    geom_raster(data = ak_df ,aes(x = x, y = y,alpha=HYP_HR_SR_W.1), show.legend=FALSE) +
+    geom_stars(data = data, alpha = 0.8) + 
     geom_sf(data = wrst, aes(), fill = NA) + 
-    labs(title = "snow:rain") +
+    labs(title = title) +
     theme_map() +
     theme(legend.position = "bottom",
           legend.key.width = unit(1, "cm"),
@@ -57,23 +55,30 @@ col.ramp = c("mediumseagreen","#fed976", "#fd8d3c", "#e31a1c", "#c6dbef",
           legend.justification = "center",
           legend.spacing = unit(0, "cm"),
           plot.title=element_text(size=12,face="bold",hjust=0.5),
-          plot.background = element_rect(colour = "black", fill=NA, size=5)) + 
-    labs(fill = paste0("snow:rain")) +
+          plot.background = element_rect(colour = bkd_col, fill=NA, size=5)) + 
+    labs(fill = "Snow regime") +
     scale_fill_manual(values = col.ramp,
                       limits = c("0.1", "0.2","0.3", "0.4","0.5", "0.6","0.7", "0.8", "0.9", "1.0")) +
   guides(fill=guide_legend(nrow=1,byrow=TRUE,label.position="top",title.position = "top",title.hjust = 0.5))
-         
-# }
+}
 
-cf1.plot <- map.plot(data=readRDS(CF1.ls),title=CFs[1],metric=long.title,col=cols[1])
-cf2.plot <- map.plot(data=readRDS(CF2.ls),title=CFs[2],metric=long.title,col=cols[2])
-cf3.plot <- map.plot(data=readRDS(CF3.ls),title=CFs[3],metric=long.title,col=cols[3])
+hist.plot <- map.plot(data=hist_cat,title=paste("Historical","1980-2016",sep="\n"),bkd_col="grey")
+cf1.plot <- map.plot(data=cf1_cat,title=paste(CFs[1],"2025-2055",sep="\n"),bkd_col=cols[1])
+cf2.plot <- map.plot(data=cf2_cat,title=paste(CFs[2],"2025-2055",sep="\n"),bkd_col=cols[2])
+cf3.plot <- map.plot(data=cf3_cat,title=paste(CFs[3],"2025-2055",sep="\n"),bkd_col=cols[3])
 
 ############## ts plots
-Hist1 = split(Hist, "time")
 
-as.data.frame(Hist1) -> c
-c2 <- gather(c,year,val,X1980.01.01:X2016.01.01) %>% 
+
+summarize_ts <- function(in_stars,crop_obj,begyr,endyr,title,bkd_col){
+crop_obj = 
+stars_crop = st_transform(in_stars, st_crs(crop_obj))  
+x1 = split(in_stars, "time")
+
+yrs=paste0("X",seq(begyr,endyr,1),".01.01")
+
+as.data.frame(x1) -> c
+c2 <- gather(c,year,val,all_of(yrs)) %>% 
   drop_na() %>% 
   mutate(Year = substr(year, 2, 5),
          categories = round(val,digits=1),
@@ -88,7 +93,23 @@ c2 <- gather(c,year,val,X1980.01.01:X2016.01.01) %>%
 
 ggplot(c2, aes(fill=cat_factor, y=percent, x=Year)) + 
   geom_bar(position="fill", stat="identity") +
-  scale_fill_manual(values = col.ramp)
+  scale_fill_manual(values = col.ramp) +
+  labs(title = title) +
+  theme(legend.position = "bottom",
+        legend.key.width = unit(1, "cm"),
+        legend.key.height = unit(.3, "cm"),
+        legend.justification = "center",
+        legend.spacing = unit(0, "cm"),
+        plot.title=element_text(size=12,face="bold",hjust=0.5),
+        plot.background = element_rect(colour = bkd_col, fill=NA, size=5)) + 
+  labs(fill = "Snow regime") +
+  # scale_fill_manual(values = col.ramp,
+                    # limits = c("0.1", "0.2","0.3", "0.4","0.5", "0.6","0.7", "0.8", "0.9", "1.0")) +
+  guides(fill=guide_legend(nrow=1,byrow=TRUE,label.position="top",title.position = "top",title.hjust = 0.5))
+}
+
+hist_ts <-summarize_ts(hist,1980,2016,title=paste("Historical","1980-2016",sep="\n"),bkd_col="grey")
+cf1_ts <-summarize_ts(cf1,2025,2055,title=paste(CFs[1],"2025-2055",sep="\n"),bkd_col=cols[1])
                     
 ts <- ggplot(df, aes(x=Year, y=(eval(parse(text=var))), group=CF, colour = CF)) +
   
@@ -112,8 +133,8 @@ ts
 
 
 #### Just maps and ts plot
-maps <- grid_arrange_shared_legend(cf1.plot, cf2.plot, cf3.plot, ncol = 3, nrow = 1, position = "bottom", 
-                                   top = textGrob(paste0("Change in ",long.title),
+maps <- grid_arrange_shared_legend(hist.plot,cf1.plot, cf2.plot, cf3.plot, ncol = 4, nrow = 1, position = "bottom",
+                                   top = textGrob(paste0("Change in ",var),
                                                   gp=gpar(fontface="bold", col="black", fontsize=16)))
 # g <- ggarrange(maps,ts, nrow=2)
 # g
